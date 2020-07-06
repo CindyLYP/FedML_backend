c6bd6a32b6894cb969269e68959002128fbea8a3
ZJU-warren <CS_wurui@163.com>
2020-07-06 10:28:33 +0800

    some objects update

--- src/main/java/com.fl.server/controller/GetValidateState.java
+++ src/main/java/com.fl.server/controller/GetValidateState.java
@@ -2,6 +2,7 @@ package com.fl.server.controller;
 
 import com.fl.server.object.Index;
 import com.fl.server.object.TrainState;
+import com.fl.server.object.ValidateState;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.ResponseBody;
@@ -9,12 +10,12 @@ import org.springframework.web.bind.annotation.RestController;
 
 @RestController
 public class GetValidateState {
-    @GetMapping("/getTrainState ")
+    @GetMapping("/getValidateState ")
     @ResponseBody
-    public TrainState GetTrainStateFunc(@RequestBody Index index) {
-        TrainState trainState = new TrainState();
-        // get train state from model
+    public ValidateState GetValidateStateFunc(@RequestBody Index index) {
+        ValidateState validateState = new ValidateState();
+        // get  validateState from model
 
-        return trainState;
+        return validateState;
     }
 }
