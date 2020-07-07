ac844c181907a6d0088b747063dc2f3fbb50775e
ZJU-warren <CS_wurui@163.com>
2020-07-07 14:57:59 +0800

    change the folder structure

--- src/main/java/com/fl/server/controller/Login.java
+++ src/main/java/com/fl/server/controller/Login.java
@@ -1,6 +1,8 @@
 package com.fl.server.controller;
 
+import com.fl.server.mapper.UserMapper;
 import com.fl.server.object.MD5;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.*;
 import com.fl.server.object.User;
 import com.fl.server.object.Message;
@@ -10,18 +12,26 @@ import java.security.NoSuchAlgorithmException;
 
 @RestController
 public class Login{
+    @Autowired
+    private UserMapper userMapper;
     @PostMapping("/login")
     @ResponseBody
-    public Message LoginCheck(@RequestBody User user) throws UnsupportedEncodingException, NoSuchAlgorithmException {
+    public Message LoginCheck(
+            @RequestParam("email") String email,
+            @RequestParam("password") String password
+    ) throws UnsupportedEncodingException, NoSuchAlgorithmException {
+
         Message message = new Message();
-        System.out.println(user.getUsername());
-        System.out.println(user.getEmail());
+        System.out.println(email);
+        System.out.println(password);
 
         // get the hash password
-        String hashPassword = MD5.EncoderByMd5(user.getPassword());
+        //        String hashPassword = MD5.EncoderByMd5(user.getPassword());
 
         // check the information
-        // status = DB_User.check(user.email, hashPassword)
+        User user = userMapper.selectByEmail(email);
+
+
         // message.setState(false);
 
         // if (status == 0){
