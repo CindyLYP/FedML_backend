9bca54506fe1f6e767bddd48e5612bc35c3a358a
ZJU-warren <CS_wurui@163.com>
2020-07-08 15:06:41 +0800

    add interceptor

--- src/main/java/com/fl/server/controller/Login.java
+++ src/main/java/com/fl/server/controller/Login.java
@@ -2,49 +2,132 @@ package com.fl.server.controller;
 
 import com.fl.server.mapper.UserMapper;
 import com.fl.server.object.MD5;
+import org.apache.coyote.Response;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.*;
 import com.fl.server.object.User;
 import com.fl.server.object.Message;
 
+import javax.servlet.http.HttpServletResponse;
 import java.io.UnsupportedEncodingException;
 import java.security.NoSuchAlgorithmException;
+import java.util.Date;
+import java.util.HashMap;
+import java.util.List;
 
 @RestController
 public class Login{
     @Autowired
     private UserMapper userMapper;
+
     @PostMapping("/login")
     @ResponseBody
-    public Message LoginCheck(
+    public HashMap<String, Object> LoginCheck(
             @RequestParam("email") String email,
             @RequestParam("password") String password
     ) throws UnsupportedEncodingException, NoSuchAlgorithmException {
 
         Message message = new Message();
-        System.out.println(email);
-        System.out.println(password);
-
         // get the hash password
-        //        String hashPassword = MD5.EncoderByMd5(user.getPassword());
+        String hashPassword = MD5.EncoderByMd5(password);
 
-        // check the information
+        // get the usr information
         User user = userMapper.selectByEmail(email);
 
+        // return message
+        if (user == null){
+            message.setState(false);
+            message.setMessage("user does not exist!");
+        }
+        else if (!user.getPassword().equals(hashPassword)){
+            message.setState(false);
+            message.setMessage("password is wrong!");
+        }
+        else {
+            message.setState(true);
+            message.setMessage("user login successfully!");
+        }
+        HashMap<String, Object> hashMap = new HashMap<String, Object>();
 
-        // message.setState(false);
-
-        // if (status == 0){
-        message.setState(true);
-        message.setMessage("user login successfully!");
-        //}
-        // else if (status == 1)
-        //      message.setMessage("user does not exist!");
-
-        // else if (status == 2)
-        //      message.setMessage("password is wrong!");
-
-        // else{message.setMessage("something wrong in there!");}
-        return message;
+        String token = MD5.EncoderByMd5((new Date()).toString());
+        hashMap.put("data", message);
+        hashMap.put("token", token);
+        return hashMap;
     }
 }
+//    @PostMapping("/GetTrainState")
+//    @ResponseBody
+//    public <----> GetTrainState(
+//            @RequestParam("trainID") String email,
+//            ) throws UnsupportedEncodingException, NoSuchAlgorithmException {
+//    }
+//    @PostMapping("/GetTrainStatus")
+//    @ResponseBody
+//    public <----> GetTrainState(
+//            @RequestParam("trainID") String email,
+//            ) throws UnsupportedEncodingException, NoSuchAlgorithmException {
+//    }
+//
+//
+//    @PostMapping("/deleteTrain")
+//    @ResponseBody
+//    public <state> DeleteTrain(
+//            @RequestParam("trainID") String email,
+//            ) throws UnsupportedEncodingException, NoSuchAlgorithmException {
+//    }
+//
+//    @PostMapping("/restartTrain")
+//    @ResponseBody
+//    public <state> RestartTrain(
+//            @RequestParam("trainID") String email,
+//            ) throws UnsupportedEncodingException, NoSuchAlgorithmException {
+//    }
+//
+//    @PostMapping("/launchTrain")
+//    @ResponseBody
+//    public <trainID, state> LaunchTrain(
+//            @RequestParam("datasetID") String email,
+//            @RequestParam("modelName") String email,
+//            @RequestParam("parameters") String email,
+//            ) throws UnsupportedEncodingException, NoSuchAlgorithmException {
+//    }
+//
+//    @PostMapping("/datasetDel")
+//    @ResponseBody
+//    public <state> DatasetDEL(
+//            @RequestParam("datasetID") String email,
+//    ) throws UnsupportedEncodingException, NoSuchAlgorithmException {
+//    }
+//
+//    @PostMapping("/datasetChoice")
+//    @ResponseBody
+//    public <datasetID, state> DatasetChoice(
+//            @RequestParam("rules") String email,
+//            @RequestParam("frac") Float password
+//    ) throws UnsupportedEncodingException, NoSuchAlgorithmException {
+//    }
+//
+//
+//
+//    @PostMapping("/datasetChoice")
+//    @ResponseBody
+//    public <datasetID, state> DatasetChoice(
+//            @RequestParam("rules") String email,
+//            @RequestParam("frac") Float password
+//    ) throws UnsupportedEncodingException, NoSuchAlgorithmException {
+//    }
+//
+//    @PostMapping("/attrFilter")
+//    @ResponseBody
+//    public <state> AttrFilter(
+//            @RequestParam("taskID") String email,
+//            @RequestParam("selects") <List<String>> password
+//    ) throws UnsupportedEncodingException, NoSuchAlgorithmException {
+//    }
+//
+//    @PostMapping("/launchTask")
+//    @ResponseBody
+//    public <taskID, state> TaskLaunch(
+//            @RequestParam("describe") String email,
+//    ) throws UnsupportedEncodingException, NoSuchAlgorithmException {
+//    }
--- src/main/java/com/fl/server/controller/Register.java
+++ src/main/java/com/fl/server/controller/Register.java
@@ -25,3 +25,5 @@ public class Register {
         return message;
     }
 }
+
+
