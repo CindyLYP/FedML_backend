7b5f701a02c811b2e0c7220f7884eb35192de5e5
ZJU-warren <CS_wurui@163.com>
2020-07-10 11:03:48 +0800

    base func interface has done

--- src/main/java/com/fl/server/controller/TrainControl.java
+++ src/main/java/com/fl/server/controller/TrainControl.java
@@ -10,6 +10,7 @@ import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.ResponseBody;
 import org.springframework.web.bind.annotation.RestController;
 
+import java.util.Date;
 import java.util.HashMap;
 
 @RestController
@@ -20,26 +21,28 @@ public class TrainControl {
     @PostMapping("/trainLaunch")
     @ResponseBody
     public HashMap<String, Object> TrainLaunch(
+        @RequestParam("taskID") String taskID,
         @RequestParam("datasetID") String datasetID,
         @RequestParam("modelName") String modelName,
         @RequestParam("parameters") String parameters
         ) {
         Train train = new Train();
-
-
-
+        train.setModelType(modelName);
+        train.setParamDim(parameters);
+        train.setTrainId("Train_" + taskID + "_"  + (new Date()).toString());
+        train.setTaskId(taskID);
 
         // add to the db
-        Boolean status = taskMapper.createTaskByEmail(task);
+        Boolean status = taskMapper.createTrainByTask(train);
 
         // return message
         Message message = new Message();
         message.setState(status);
-        message.setMessage(status? "launch task successfully" : "something is wrong here");
+        message.setMessage(status? "launch train successfully" : "something is wrong here");
 
         HashMap<String, Object> hashMap = new HashMap<String, Object>();
         hashMap.put("message", message);
-        hashMap.put("taskID", task.getTaskId());
+        hashMap.put("trainID", train.getTrainId());
         return hashMap;
 
     }
@@ -59,4 +62,63 @@ public class TrainControl {
         return message;
     }
 
+    @PostMapping("/trainRestart")
+    @ResponseBody
+    public Message TrainRestart(
+        @RequestParam("trainID") String trainID
+    ) {
+
+
+        boolean status = true;
+
+        // return message
+        Message message = new Message();
+        message.setState(status);
+        message.setMessage(status? "train delete successfully" : "something is wrong here");
+        return message;
+    }
+
+    @PostMapping("/getTrainState")
+    @ResponseBody
+    public HashMap<String, Object> GetTrainState(
+        @RequestParam("trainID") String trainID
+    ) {
+        Boolean status = true;
+
+        // return message
+        Message message = new Message();
+        message.setState(status);
+        message.setMessage(status? "get train state successfully" : "something is wrong here");
+
+        HashMap<String, Object> hashMap = new HashMap<String, Object>();
+        hashMap.put("message", message);
+        hashMap.put("state", "interface works");
+        return hashMap;
+    }
+
+    @PostMapping("/getTrainBatchInfo")
+    @ResponseBody
+    public HashMap<String, Object> GetTrainBatchInfo(
+            @RequestParam("trainID") String trainID
+    ) {
+        Boolean status = true;
+
+        // return message
+        Message message = new Message();
+        message.setState(status);
+        message.setMessage(status? "get train batch info successfully" : "something is wrong here");
+
+        HashMap<String, Object> hashMap = new HashMap<String, Object>();
+        hashMap.put("message", message);
+        hashMap.put("n_rounds", 10);
+        hashMap.put("recent_loss", 0.11);
+
+        HashMap<String, Double> recent_metrics = new HashMap<String, Double>();
+        recent_metrics.put("auc", 0.5);
+        recent_metrics.put("ks", 0.2);
+
+        hashMap.put("recent_metrics", recent_metrics);
+
+        return hashMap;
+    }
 }
