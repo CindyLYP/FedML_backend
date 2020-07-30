ef94655848e4a11653d44f1af58b2053b19b35e4
CindyLYP <scying@zju.edu.cn>
2020-07-30 10:34:33 +0800

    modify scene

--- src/main/java/com/fl/server/pojo/Scene.java
+++ src/main/java/com/fl/server/pojo/Scene.java
@@ -1,11 +1,15 @@
 package com.fl.server.pojo;
 
+import java.util.ArrayList;
+
 public class Scene {
     private int id;
     private String institution;
     private String sceneName;
     private String target;
     private String description;
+    private ArrayList<Integer> valueList;
+    private ArrayList<String> keyList;
 
     public int getId() {
         return id;
@@ -46,4 +50,43 @@ public class Scene {
     public void setDescription(String description) {
         this.description = description;
     }
+
+    public ArrayList<Integer> getValueList() {
+        return valueList;
+    }
+
+    public void setValueList(ArrayList<Integer> valueList) {
+        this.valueList = valueList;
+    }
+
+    public ArrayList<String> getKeyList() {
+        return keyList;
+    }
+
+    public void setKeyList(ArrayList<String> keyList) {
+        this.keyList = keyList;
+    }
+
+    public void StringToDict(){
+        this.keyList = new ArrayList<>();
+        this.valueList = new ArrayList<>();
+        for (String attr :this.description.split("#")){
+            String[] str = attr.split(":");
+            this.valueList.add(Integer.parseInt(str[0]));
+            this.keyList.add(str[1]);
+        }
+    }
+
+    public void dictToString(){
+        int i=0;
+        this.description="";
+        for(; i < (this.keyList.size() - 1); i++){
+            this.description += String.valueOf(this.valueList.get(i))+":"+
+                    this.keyList.get(i)+"#";
+        }
+        i = this.keyList.size()-1;
+        this.description += String.valueOf(this.valueList.get(i))+":"+
+                this.keyList.get(i);
+    }
+
 }
